56f6a861c09362bcd93c1d74f80dd7a5ea89c1e0
elbiesmith <elbie.smith@gmail.com>
2021-07-31 11:39:40 -0500

    Added Mortgage Master

---
+++ Controllers/LoanController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MVCChallenges.Helpers;
+using MVCChallenges.Models;
+using System.Diagnostics;
+
+namespace MVCChallenges.Controllers
+{
+    public class LoanController : Controller
+    {
+        private readonly ILogger<LoanController> _logger;
+
+        public LoanController(ILogger<LoanController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult App()
+        {
+            Loan loan = new();
+            loan.Payment = 0.0m;
+            loan.TotalInterest = 0.0m;
+            loan.TotalCost = 0.0m;
+            loan.Rate = 3.5m;
+            loan.Amount = 15000m;
+            loan.Term = 60;
+
+            return View(loan);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult App(Loan loan)
+        {
+            var loanHelper = new LoanHelper();
+
+            Loan newLoan = loanHelper.GetPayments(loan);
+
+            return View(newLoan);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Helpers/LoanHelper.cs
@@ -0,0 +1,65 @@
+﻿using MVCChallenges.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCChallenges.Helpers
+{
+    public class LoanHelper
+    {
+        public Loan GetPayments(Loan loan)
+        {
+            loan.Payment = CalcPayment(loan.Amount, loan.Rate, loan.Term);
+
+            var balance = loan.Amount;
+            var totalInterest = 0.0m;
+            var monthlyInterest = 0.0m;
+            var monthlyPrincipal = 0.0m;
+            var monthlyRate = CalcMonthlyRate(loan.Rate);
+
+            for (int i = 1; i <= loan.Term; i++)
+            {
+                monthlyInterest = CalcMonthlyInterest(balance, monthlyRate);
+                totalInterest += monthlyInterest;
+                monthlyPrincipal = loan.Payment - monthlyInterest;
+                balance -= monthlyPrincipal;
+
+                LoanPayment loanPayment = new();
+                loanPayment.Month = i;
+                loanPayment.Payment = loan.Payment;
+                loanPayment.MonthlyPrincipal = monthlyPrincipal;
+                loanPayment.MonthlyInterest = monthlyInterest;
+                loanPayment.TotalInterest = totalInterest;
+                loanPayment.Balance = balance;
+
+                loan.Payments.Add(loanPayment);
+            }
+
+            loan.TotalInterest = totalInterest;
+            loan.TotalCost = loan.Amount + totalInterest;
+
+            return loan;
+        }
+
+        private decimal CalcPayment(decimal amount, decimal rate, int term)
+        {
+            var rateD = Convert.ToDouble(CalcMonthlyRate(rate));
+            var amountD = Convert.ToDouble(amount);
+
+            var paymentD = (amountD * rateD) / (1 - Math.Pow(1 + rateD, -term));
+
+            return Convert.ToDecimal(paymentD);
+        }
+
+        private decimal CalcMonthlyRate(decimal rate)
+        {
+            return rate / 1200;
+        }
+
+        private decimal CalcMonthlyInterest(decimal balance, decimal monthlyRate)
+        {
+            return balance * monthlyRate;
+        }
+    }
+}
